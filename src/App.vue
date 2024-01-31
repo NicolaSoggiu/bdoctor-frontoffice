48e0f3bdafec0d8b1fe8b4a14ad407a01cb62d7f
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2024-01-31 17:24:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+      profilesData: [],
+      profiloUno: [],
+      is404: false,
+    };
+  },
+  methods: {
+    getApartment() {
+      try {
+        axios.get(this.store.baseUrl + "profiles").then((response) => {
+          this.profilesData = response.data;
+          this.profiloUno = response.data[0];
+          console.log("response", this.profilesData);
+          console.log("profiloUno", this.profiloUno);
+        });
+      } catch (error) {
+        console.log("error", this.is404);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-decoration-underline">Hello</h1>
+  <div>{{ getApartment() }}</div>
+</template>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
